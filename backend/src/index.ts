72672801315795d68275871e18d144dccb93a1df
sivakumar-16 <sivap4817@gmail.com>
2024-09-06 12:01:31 +0530

    Initial setup

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+const PORT = 3000
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send('successfully conected')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is conected on ${PORT} 🫣`);
+    
+})
+
